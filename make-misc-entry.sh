a4cf1c421bafc1aa361e1584d3de8ae9e82877cb
Keith Stellyes <keithastellyes@gmail.com>
2016-08-24 19:18:06 -0700

    Added bash script that updates project

---
+++ make-misc-entry.sh
@@ -0,0 +1,13 @@
+#Personal script for making new entries into the misc repo
+#SYNTAX:
+#SCRIPT file language problem
+FILE=$1
+LANGUAGE=$2
+PROBLEM=$3
+
+PROJECT_DIRECTORY="/home/keith/Code/Portfolio/Misc/misc"
+BY_LANGUAGE_DIR="$PROJECT_DIRECTORY/by-language/$2/"
+BY_PROBLEM_DIR="$PROJECT_DIRECTORY/by-problem/$3/$2/"
+
+cp "$1" "$BY_LANGUAGE_DIR"
+cp "$1" "$BY_PROBLEM_DIR"
